ca3dee48dd063cfcf3d6c3f0350687684be824b9
Danpa_cho <danpa725@cau.ac.kr>
2022-08-14 18:11:21 +0900

    Add: vite.config for lib build .

---
+++ vite.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from "vite";
+
+import react from "@vitejs/plugin-react";
+import dts from "vite-plugin-dts";
+
+import { resolve } from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [react(), dts()],
+    build: {
+        lib: {
+            entry: resolve(__dirname, "packages/index.ts"),
+            name: "jotaish",
+            formats: ["es", "umd"],
+            fileName: (format: string) => `jotaish.${format}.js`,
+        },
+        rollupOptions: {
+            external: ["react", "jotai"],
+            output: {
+                globals: {
+                    react: "React",
+                    jotai: "jotai",
+                },
+            },
+        },
+    },
+});
