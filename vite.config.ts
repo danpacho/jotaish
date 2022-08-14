56f3e195bfeb05d353775ee0a643f910398d9574
Danpa_cho <danpa725@cau.ac.kr>
2022-08-14 21:51:54 +0900

    Remove: @vitejs/plugin-react .

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
 import { defineConfig } from "vite";
 
-import react from "@vitejs/plugin-react";
 import dts from "vite-plugin-dts";
 
 import { resolve } from "path";
 
 export default defineConfig({
-    plugins: [react(), dts()],
+    plugins: [dts()],
     build: {
         lib: {
             entry: resolve(__dirname, "packages/index.ts"),
